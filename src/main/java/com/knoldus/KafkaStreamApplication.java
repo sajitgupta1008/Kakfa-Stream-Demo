85bc29630b43b1f4152c9c29bc2df132c12539a8
Himani Arora <himani.arora@knoldus.in>
2017-10-11 10:05:09 +0530

    Update Kstream demo

--- src/main/java/com/knoldus/KafkaStreamApplication.java
+++ src/main/java/com/knoldus/KafkaStreamApplication.java
@@ -1,29 +1,21 @@
 package com.knoldus;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.Properties;
-import java.util.logging.Logger;
-
 import demo.Location;
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.specific.SpecificRecord;
-import org.apache.kafka.common.serialization.Serde;
+import org.apache.avro.specific.SpecificData;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
-import org.apache.kafka.streams.kstream.ValueMapper;
-import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 
-import static sun.plugin.javascript.navig.JSType.Location;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+import java.util.logging.Logger;
 
 public class KafkaStreamApplication {
 
@@ -44,35 +36,42 @@ public class KafkaStreamApplication {
 
     void transform() {
 
-        Properties streamConfiguration = getStreamConfiguration();
-        KStreamBuilder builder = new KStreamBuilder();
-
-        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
-                "http://localhost:8081");
-
-        /*  final Map<String, String>
-                serdeProps =
-                Collections.singletonMap("schema.registry.url", "http://localhost:8081");*/
 
         final CachedSchemaRegistryClient
                 schemaRegistry =
                 new CachedSchemaRegistryClient("http://localhost:8081", 100);
 
-        final Serde<Location> valueSpecificAvroSerde = new SpecificAvroSerde<>();
-        valueSpecificAvroSerde.configure(serdeConfig, false);
+        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
+                "http://localhost:8081");
+
+        Properties streamConfiguration = getStreamConfiguration();
+        KStreamBuilder builder = new KStreamBuilder();
+
+        final GenericAvroSerde GenericRecordSerde = new GenericAvroSerde(schemaRegistry, serdeConfig);
+        GenericRecordSerde.configure(serdeConfig, false);
+
+        final SpecificAvroSerde<Location> locationEventSerde = new SpecificAvroSerde<>(schemaRegistry, serdeConfig);
+        locationEventSerde.configure(serdeConfig, false);
 
         //current  //  KStream<String, Location> kStream = builder.stream(null, valueSpecificAvroSerde, "orders-topic");
-        KStream<String, GenericRecord> kStream = builder.stream(null, new MyAvroSerde(schemaRegistry, serdeConfig), "orders-topic");
+        KStream<String, GenericRecord> kStream = builder.stream(null, new com.knoldus.GenericAvroSerde(schemaRegistry, serdeConfig),
+                "orders-topic");
         //     builder.stream(null, new GenericAvroSerde(schemaRegistry), "");
 
         // KStream<Long, SpecificRecord> kStream = builder.stream( "orders-topic");
 
-        KStream<String, Location> newStream = kStream.map((k, v) -> new KeyValue<String, Location>(k,
-                new Location((String) v.get("created"), (String) v.get("id") + " knoldus")));
-
+        KStream<String, Location> newStream = kStream.map((k, v) -> {
+                    Location event = (Location) SpecificData.get().deepCopy(Location.SCHEMA$, v);
+                    System.out.println("date ---->>>> " + event.getCreated() + " ----- " + event.getId());
+                    return new KeyValue<>(k,
+                            new Location((String) event.getCreated().toString(), (String) event.getId().toString() + " "
+                                    + "knoldus"));
+                }
+        );
+        newStream.print("output is ");
 
         //  newStream.to(null,valueSpecificAvroSerde,"outtopic-3");
-        newStream.to("outtopic-3");
+        newStream.to(Serdes.String(), locationEventSerde, "outtopic-3");
         KafkaStreams stream = new KafkaStreams(builder, streamConfiguration);
         stream.start();
     }
